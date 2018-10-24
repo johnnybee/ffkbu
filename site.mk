4237a9d1272db933bc30e21fe14e56a897573cb7
Thomas Winter <johnny.bee@gmx.de>
2018-10-24 10:34:43 +0200

    Update site.mk

--- site.mk
+++ site.mk
@@ -253,7 +253,7 @@ GLUON_tp-link-tl-wdr4900-v1_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(TOOLS_PACKA
 #               to decide if a version is newer or not.
 
 #DEFAULT_GLUON_RELEASE := 0.6+mstr$(shell date '+%Y%m%d')
-DEFAULT_GLUON_RELEASE := v2018.1-Wireguard
+DEFAULT_GLUON_RELEASE := v2018.1.1-Wireguard
 
 
 ##      GLUON_RELEASE
@@ -264,7 +264,7 @@ DEFAULT_GLUON_RELEASE := v2018.1-Wireguard
 #                       gluon-ff%site_code%-23.42+5-%router_model%.bin
 
 # Allow overriding the release number from the command line
-GLUON_RELEASE ?= v2018.1-Wireguard
+GLUON_RELEASE ?= v2018.1.1-Wireguard
 
 # Default priority for updates.
 GLUON_PRIORITY ?= 0
