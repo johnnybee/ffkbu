fda584aed4f5be1a33cadc0d337f36ad6e0bd744
Simon Müller <simon.f.mueller@gmail.com>
2019-09-14 13:45:28 +0200

    Update site.mk

--- site.mk
+++ site.mk
@@ -181,7 +181,7 @@ GLUON_tp-link-tl-wdr4900-v1_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(TOOLS_PACKA
 #               to decide if a version is newer or not.
 
 #DEFAULT_GLUON_RELEASE := 0.6+mstr$(shell date '+%Y%m%d')
-DEFAULT_GLUON_RELEASE := v2018.2.2-Wireguard
+DEFAULT_GLUON_RELEASE := v2018.2.3-Wireguard
 
 
 ##      GLUON_RELEASE
