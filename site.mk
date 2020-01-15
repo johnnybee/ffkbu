8680030a39638b4022c4d547641c806f5ace914c
Thomas Winter <johnny.bee@gmx.de>
2020-01-15 10:18:12 +0100

    update gluon release

--- site.mk
+++ site.mk
@@ -169,7 +169,7 @@ GLUON_TLWDR4900_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(TOOLS_PACKAGES) $(USB_P
 #               to decide if a version is newer or not.
 
 #DEFAULT_GLUON_RELEASE := 0.6+mstr$(shell date '+%Y%m%d')
-DEFAULT_GLUON_RELEASE := v2019.1-FastD
+DEFAULT_GLUON_RELEASE := v2019.1.1-FastD
 
 
 ##      GLUON_RELEASE
@@ -180,7 +180,7 @@ DEFAULT_GLUON_RELEASE := v2019.1-FastD
 #                       gluon-ff%site_code%-23.42+5-%router_model%.bin
 
 # Allow overriding the release number from the command line
-GLUON_RELEASE ?= v2019.1-FastD
+GLUON_RELEASE ?= v2019.1.1-FastD
 
 # Default priority for updates.
 GLUON_PRIORITY ?= 0
